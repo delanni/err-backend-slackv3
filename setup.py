2144ef5a87ceef246eb3b2be95d0fc2f4486c2fc
Alex Szabo <alex.szabo@prezi.com>
2022-09-13 13:37:06 +0200

    Idk what exactly I'm doing

--- setup.py
+++ setup.py
@@ -3,11 +3,6 @@ from setuptools import setup, find_packages
 setup(
   name='err-backend-slackv3',
   version='0.1.0',
-
-  # Use one of the below approach to define package and/or module names:
-
-  # this approach automatically finds out all directories (packages) - those must contain a file named __init__.py (can be empty)
-  packages=find_packages(),
-
+  packages=[*find_packages(), '.'],
   description="Slack(v3) backend for Errbot"
 )
